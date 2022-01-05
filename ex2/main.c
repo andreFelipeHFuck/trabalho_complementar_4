82afe3fe5e32cf68a11ccdbd0d9679165f15e03d
andreFelipeHFuck <neoandfufe42@gmail.com>
2022-01-05 10:41:03 -0300

    ex2

---
+++ ex2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int n, n_entrada;
+    int i, j, k;
+    FILE *arq;
+
+    arq = fopen("entrada.txt", "wt");
+    if(arq == NULL){
+        printf("Erro no arquivo de entrada\n");
+        return 0;
+    }
+
+    printf("N: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+        n_entrada = rand() % 20;
+        fprintf(arq, "%d ", n_entrada);
+        for(j = 0; j < n_entrada; j++){
+            fprintf(arq, "%d ", rand() % -10);
+        }
+        fprintf(arq, "\n");
+    }
+    fprintf(arq, "0");
+
+    fclose(arq);
+    return 0;
+}
